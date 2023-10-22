e69fc84b627fe50b641d8c7e1dae692303dbb08e
ChamXE <xuaner11221@outlook.com>
2023-10-22 22:04:28 +0800

    Add generate button

--- src/pages/project/project.tsx
+++ src/pages/project/project.tsx
@@ -1,19 +1,19 @@
-import { Project } from "@/models/project";
-import { getProject, getServer, getURL } from '@/components/global';
-import { useState, useEffect } from "react";
+import {Project} from "@/models/project";
+import {getProject, getServer, getURL} from '@/components/global';
+import React, {useState, useEffect} from "react";
 import useToken from "@/components/app/useToken";
 import axios from "axios";
-import { DataGrid, GridColDef, useGridApiRef } from "@mui/x-data-grid";
+import {DataGrid, GridColDef, GridRenderCellParams, useGridApiRef} from "@mui/x-data-grid";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Server } from "@/models/device";
-import { Box, Button, Container, IconButton, Typography } from "@mui/material";
+import {Server} from "@/models/device";
+import {Box, Button, Container, IconButton, Typography} from "@mui/material";
 import CreateEditProject from "@/components/project/createEditProject";
 import ViewRS from "@/components/project/viewRS";
 import ViewNonRS from "@/components/project/viewNonRS";
 
-function ProjectList () {
-    const { userToken } = useToken();
+function ProjectList() {
+    const {userToken} = useToken();
     const apiRef = useGridApiRef();
     const [project, setProject] = useState<Project[] | null>(null);
     const [server, setServer] = useState<Server[] | null>(null);
@@ -22,15 +22,19 @@ function ProjectList () {
     const [routerSwitch, setRouterSwitch] = useState<React.ReactElement | null>(null);
     const [switchSwitch, setSwitchSwitch] = useState<React.ReactElement | null>(null);
     const [switchHost, setSwitchHost] = useState<React.ReactElement | null>(null);
-    
+
     const column: GridColDef[] = [
-        { field: 'projectid', headerName: 'Project ID', type: 'string', flex: 1},
-        { field: 'projectname', headerName: 'Project Name', type: 'string', flex: 1},
+        {field: 'projectid', headerName: 'Project ID', type: 'string', flex: 1},
+        {field: 'projectname', headerName: 'Project Name', type: 'string', flex: 1},
         {
-            field: 'routerswitch', headerName: 'Router - Switch Connection', type: 'string', flex: 1, renderCell: () => (
+            field: 'routerswitch',
+            headerName: 'Router - Switch Connection',
+            type: 'string',
+            flex: 1,
+            renderCell: () => (
                 <Button
                     variant="contained"
-                    sx={{ marginLeft: '1rem' }}
+                    sx={{marginLeft: '1rem'}}
                     onClick={handleOpenRS}
                 >
                     View
@@ -38,10 +42,14 @@ function ProjectList () {
             )
         },
         {
-            field: 'switchswitch', headerName: 'Switch - Switch Connection', type: 'string', flex: 1, renderCell: () => (
+            field: 'switchswitch',
+            headerName: 'Switch - Switch Connection',
+            type: 'string',
+            flex: 1,
+            renderCell: () => (
                 <Button
                     variant="contained"
-                    sx={{ marginLeft: '1rem' }}
+                    sx={{marginLeft: '1rem'}}
                     onClick={handleOpenSS}
                 >
                     View
@@ -52,7 +60,7 @@ function ProjectList () {
             field: 'switchhost', headerName: 'Switch - Host Connection', type: 'string', flex: 1, renderCell: () => (
                 <Button
                     variant="contained"
-                    sx={{ marginLeft: '1rem' }}
+                    sx={{marginLeft: '1rem'}}
                     onClick={handleOpenSH}
                 >
                     View
@@ -63,16 +71,30 @@ function ProjectList () {
             field: 'null', headerName: 'Action', flex: 1, renderCell: () => (
                 <Box component="span" display="flex">
                     <IconButton onClick={handleEdit}>
-                        <EditIcon />
+                        <EditIcon/>
                     </IconButton>
-                    <IconButton sx={{ ml: "1rem" }} onClick={handleDelete}>
-                        <DeleteIcon />
+                    <IconButton sx={{ml: "1rem"}} onClick={handleDelete}>
+                        <DeleteIcon/>
                     </IconButton>
                 </Box>
             )
-        }
+        },
+        {
+            field: 'generated',
+            headerName: 'Generate Hostfile',
+            flex: 1,
+            renderCell: (params: GridRenderCellParams<any, boolean>) => (
+                <Button
+                    variant="contained"
+                    sx={{marginLeft: '1rem'}}
+                    onClick={handleGenerateProject}
+                    disabled={params.value}
+                >
+                    Generate
+                </Button>
+            )
+        },
     ];
-
     const handleNewDataIncoming = () => {
         setNewData((prev) => prev + 1);
     }
@@ -84,7 +106,7 @@ function ProjectList () {
     const handleOpenRS = () => {
         if (apiRef.current.getSelectedRows().size) {
             const row = apiRef.current.getSelectedRows().values().next().value;
-            setRouterSwitch(<ViewRS resetRS={resetRS} project={row} />);
+            setRouterSwitch(<ViewRS resetRS={resetRS} project={row}/>);
         }
     }
 
@@ -92,10 +114,10 @@ function ProjectList () {
         setRouterSwitch(null);
     }
 
-    const handleOpenSS = () => { 
+    const handleOpenSS = () => {
         if (apiRef.current.getSelectedRows().size) {
             const row = apiRef.current.getSelectedRows().values().next().value;
-            setSwitchSwitch(<ViewNonRS connectionType='switchSwitch' resetNonRS={resetSS} project={row} />);
+            setSwitchSwitch(<ViewNonRS connectionType='switchSwitch' resetNonRS={resetSS} project={row}/>);
         }
     }
 
@@ -103,10 +125,10 @@ function ProjectList () {
         setSwitchSwitch(null);
     }
 
-    const handleOpenSH = () => { 
+    const handleOpenSH = () => {
         if (apiRef.current.getSelectedRows().size) {
             const row = apiRef.current.getSelectedRows().values().next().value;
-            setSwitchHost(<ViewNonRS connectionType='switchHost' resetNonRS={resetSH} project={row} />);
+            setSwitchHost(<ViewNonRS connectionType='switchHost' resetNonRS={resetSH} project={row}/>);
         }
     }
 
@@ -116,10 +138,11 @@ function ProjectList () {
 
     const handleEdit = async () => {
         const row = apiRef.current.getSelectedRows();
-        if(row.size) {
+        if (row.size) {
             const project: Project = row.values().next().value
             setEdit(
-                <CreateEditProject resetEdit={resetEdit} newDataIncoming={handleNewDataIncoming} server={server} project={project} />
+                <CreateEditProject resetEdit={resetEdit} newDataIncoming={handleNewDataIncoming} server={server}
+                                   project={project}/>
             );
         }
     }
@@ -132,8 +155,22 @@ function ProjectList () {
                 const result = await deleteProject(row.projectid);
                 if (result) setNewData((prev) => prev - 1);
             }
+        } else alert("Please select row!");
+    }
+
+    const handleGenerateProject = async () => {
+        if (apiRef.current.getSelectedRows().size) {
+            const row = apiRef.current.getSelectedRows().values().next().value;
+            if (row.generated) return alert('This project has been generated!');
+            const sureGenerate = window.confirm(`Are you sure you want to generate the hostfile for the project ${row.projectname}? This action is irreversible and you cannot do it again!`);
+            if (sureGenerate) {
+                const result = await generateProject(row.projectid);
+                if (result) {
+                    alert('Project generated!');
+                    setNewData((prev) => prev - 1);
+                } else return alert('Project creation failed!');
+            }
         }
-        else alert("Please select row!");
     }
 
     const generateTable = (project: Project[]) => (
@@ -142,10 +179,10 @@ function ProjectList () {
             columns={column}
             initialState={{
                 pagination: {
-                    paginationModel: { page: 0, pageSize: 5 },
+                    paginationModel: {page: 0, pageSize: 5},
                 },
                 sorting: {
-                    sortModel: [{ field: 'projectid', sort: 'asc' }],
+                    sortModel: [{field: 'projectid', sort: 'asc'}],
                 },
             }}
             pageSizeOptions={[5, 10]}
@@ -165,7 +202,7 @@ function ProjectList () {
     async function deleteProject(projectId: number): Promise<number> {
         try {
             const response = await axios.delete(`${getURL()}/project/${projectId}`);
-            if (!response.data.result) return 0;
+            if (!response.data.data.result) return 0;
             return 1;
         } catch (e) {
             console.error(e.message);
@@ -173,7 +210,19 @@ function ProjectList () {
             return 0;
         }
     }
-    
+
+    async function generateProject(projectId: number): Promise<number> {
+        try {
+            const response = await axios.post(`${getURL()}/project/generateProject/${projectId}`);
+            if (response.data.data.result) return 1;
+            return 0;
+        } catch (e) {
+            console.error(e.message);
+            alert('Project generation failed!');
+            return 0;
+        }
+    }
+
     useEffect(() => {
         const CancelToken = axios.CancelToken;
         const source = CancelToken.source();
@@ -190,7 +239,7 @@ function ProjectList () {
 
         loadProject();
         loadServer();
-    
+
         return () => {
             source.cancel()
         }
@@ -199,17 +248,17 @@ function ProjectList () {
     return (
         <Container
             component="main"
-            sx={{ mt: '1.5rem', ml: '3rem', maxWidth: '95vw !important' }}
+            sx={{mt: '1.5rem', ml: '3rem', maxWidth: '95vw !important'}}
         >
             <Box component="span" mb="1rem" display="flex" alignItems="center">
                 <Typography fontSize='1.5rem'>Project</Typography>
-                <CreateEditProject newDataIncoming={handleNewDataIncoming} server={server} />
+                <CreateEditProject newDataIncoming={handleNewDataIncoming} server={server}/>
             </Box>
-            { project && generateTable(project) }
-            { edit }
-            { routerSwitch }
-            { switchSwitch }
-            { switchHost }
+            {project && generateTable(project)}
+            {edit}
+            {routerSwitch}
+            {switchSwitch}
+            {switchHost}
         </Container>
     );
 }
